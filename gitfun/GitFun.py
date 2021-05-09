6def5e597b60c38be82c9a62359dea1a18f3ee9f
memetics19 <shreeda.bhat@yahoo.com>
2021-05-09 13:42:50 +0530

    removed the hardcoded value

--- gitfun/GitFun.py
+++ gitfun/GitFun.py
@@ -13,7 +13,7 @@ def init_push(url:str,m:str) -> str:
     """
 
     #
-    cmd_remote = "git remote add test19 {0}".format(url)
+    cmd_remote = "git remote add origin {0}".format(url)
     cmd_commit = "git commit -m {0}".format(m)
     print(subprocess.getoutput("git init"))
     subprocess.getoutput(cmd_remote)
@@ -25,7 +25,7 @@ def init_push(url:str,m:str) -> str:
             print(subprocess.getoutput("git commit -m initial commit"))
     except Exception as ex:
         print(ex)
-    return subprocess.getoutput("git push  test19 master ")
+    return subprocess.getoutput("git push  origin master ")
 
 
 
