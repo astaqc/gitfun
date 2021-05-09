392a57b6aac7a8d3b930a8f8107dbdb0832ba220
memetics19 <shreeda.bhat@yahoo.com>
2021-05-09 23:42:25 +0530

    constants and push branch command

--- gitfun/GitFun.py
+++ gitfun/GitFun.py
@@ -1,7 +1,14 @@
 import subprocess
 
 
-def init_push(url: str, m: str) -> str:
+def constants(url:str,m:str,b:str) -> str:
+    cmd_remote = "git remote add origin {0}".format(url)
+    cmd_commit = "git commit -m {0}".format(m)
+    cmd_branch = "git checkout -b {0}".format(b)
+    cmd_push = "git push origin {0}".format(b)
+    return cmd_remote,cmd_push,cmd_branch,cmd_commit
+
+def init_push(cmd_remote,cmd_commit) -> str:
     """
 
     :param url:str, required
@@ -11,9 +18,6 @@ def init_push(url: str, m: str) -> str:
     which pushes all the changes to repo
     """
 
-    #
-    cmd_remote = "git remote add origin {0}".format(url)
-    cmd_commit = "git commit -m {0}".format(m)
     print(subprocess.getoutput("git init"))
     subprocess.getoutput(cmd_remote)
     print(subprocess.getoutput("git add ."))
@@ -31,11 +35,12 @@ def status():
     return subprocess.getoutput("git status")
 
 
-def push_branch(commit_message, pull_branch, push_branch) -> str:
+def push_branch(cmd_commit,cmd_branch) -> str:
     try:
-        subprocess.getoutput("git pull origin {0}").format(pull_branch)
+        if
+        subprocess.getoutput(cmd_branch)
         subprocess.getoutput("git add .")
-        subprocess.getoutput("git commit -m {0}").format(commit_message)
+        subprocess.getoutput(cmd_commit)
         subprocess.getoutput("git push origin {0}").fomart(push_branch)
 
     except Exception as ex:
