5dfe392678e13359d9d3f24d9078399942cce74e
Shreeda Bhat <shreeda.bhat@yahoo.com>
2021-06-07 21:21:50 +0530

    refactored

--- gitfun/GitFun.py
+++ gitfun/GitFun.py
@@ -8,28 +8,6 @@ def constants(url:str,m:str,b:str) -> str:
     cmd_push = "git push origin {0}".format(b)
     return cmd_remote,cmd_commit,cmd_branch,cmd_push
 
-# def init_push(cmd_remote,cmd_commit) -> str:
-#     """
-#
-#     :param url:str, required
-#     The url is passed to add a remote repo link for the git
-#     :return:
-#     str  subprocess.run("git push --set-upstream origin master ")
-#     which pushes all the changes to repo
-#     """
-#
-#     print(subprocess.getoutput("git init"))
-#     subprocess.getoutput(cmd_remote)
-#     print(subprocess.getoutput("git add ."))
-#     try:
-#         if m == type(str):
-#             subprocess.getoutput(cmd_commit)
-#         elif m != type(str):
-#             subprocess.getoutput("git commit -m initial commit")
-#     except Exception as ex:
-#         print(ex)
-#     return subprocess.getoutput("git push --set-upstream origin master ")
-
 
 def status():
     return subprocess.getoutput("git status")
